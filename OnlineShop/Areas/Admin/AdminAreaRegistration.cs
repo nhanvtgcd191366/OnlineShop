50dee2aa1c4f61e3a5cdb6eff18baa8d3f3834e6
Nhan's Macbook <nhanvtgcd191366@fpt.edu.vn>
2021-11-28 17:21:18 +0700

    Add project files.

---
+++ OnlineShop/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                //Đường dẫn vào trang của Admin
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", controller="Home", id = UrlParameter.Optional },
+                new string[] { "OnlineShop.Areas.Admin.Controllers" }
+            );
+        }
+    }
+}
